60ed572ad27bdf2bcad9761440c7869063142f28
Jonathan Boeckel <jonathanboeckel1996@gmail.com>
2018-01-07 20:36:51 +0100

    Added MansNotRobot

---
+++ mansnotrobot.user.js
@@ -0,0 +1,21 @@
+// ==UserScript==
+// @name         Captcha's not hot
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Replaces the text of Google reCAPTCHA's with Man's not robot
+// @author       JonnyB
+// @match        *://*/*
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    window.onload = init;
+
+    function init() {
+        var item = document.getElementById("recaptcha-anchor-label");
+        //window.alert(item.innerHTML);
+        if (item) {
+            item.innerHTML = "Man's not robot";
+        }
+    }
+})();
