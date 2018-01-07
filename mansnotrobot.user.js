f49b5359efdfbabaded303132c17e142059f3461
Jonathan Boeckel <jonathanboeckel1996@gmail.com>
2018-01-07 21:05:57 +0100

    Updated MansNotHot
    Added license and bumped version number

--- mansnotrobot.user.js
+++ mansnotrobot.user.js
@@ -1,10 +1,11 @@
 // ==UserScript==
 // @name         Captcha's not hot
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      0.2
 // @description  Replaces the text of Google reCAPTCHA's with Man's not robot
 // @author       JonnyB
 // @match        *://*/*
+// @license MIT
 // ==/UserScript==
 
 var text = "Man's not robot";
